2d457ff8ee99796fcc6f837650f75d6c17f9c201
Matt <matthewpeterein@gmail.com>
2024-06-15 12:27:13 -0500

    🚀 Initial Commit

---
+++ swift/statusly/statusly/statuslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  statuslyApp.swift
+//  statusly
+//
+//  Created by Matthew Peterein on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct statuslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
