e1a1fdf1aa87d9d052c9f62bea36e9e888a19f5c
Matt <matthewpeterein@gmail.com>
2024-06-28 14:50:20 -0500

    🚀

--- swift/statusly/statusly/ContentView.swift
+++ swift/statusly/statusly/ContentView.swift
@@ -1,22 +1,47 @@
-//
-//  ContentView.swift
-//  statusly
-//
-//  Created by Matthew Peterein on 6/15/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName: String = ""
+    @State private var password: String = ""
+    @State private var availability : String = "Available"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome!")
+            TextField("Username:", text:$userName)
+            SecureField("Password:", text:$password)
+            //Button"Get Status", action: () -> fetchStatus(completionHandler: handleStatusResponse))
+            Button {
+                fetchStatus(completionHandler: handleStatusResponse)
+            } label: {
+                Text("Get Status")
+                    .padding(20)
+            }
+            
+            Text(availability)
+            
         }
         .padding()
     }
+        
+    func fetchStatus(completionHandler: @escaping (String) -> Void) {//TODO: This would go somewhere else
+        // Setup the variable lotsOfFilms
+        var status: String = "Hi Mom"
+          
+        // Call the API with some code
+          
+        // Using data from the API, assign a value to lotsOfFilms
+          
+        // Give the completion handler the variable, lotsOfFilms
+        completionHandler(status)
+    }
+        
+        func handleStatusResponse(with status: String) -> Void {
+            self.availability = status
+        }
 }
 
 #Preview {
